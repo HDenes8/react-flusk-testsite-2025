b0d2efb38a9161b0404cc1e480200bdfed00d8a1
HDenes <denes.horvath.email@gmail.com>
2025-03-28 13:08:35 +0100

    navbar 2.0 and main load to login page fixed, updated package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SignUp from './SignUp';
 import Login from './login';
 import MainPage from './pages/MainPage';
@@ -11,64 +8,9 @@ import './styles.css';
 function App() {
   return (
     <Router>
-      <div className="global-navbar">
-        <nav>
-          <ul>
-            <li>
-              <NavLink
-                to="/"
-                className={({ isActive }) => (isActive ? 'active' : '')}
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/about"
-                className={({ isActive }) => (isActive ? 'active' : '')}
-              >
-                About
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/contact"
-                className={({ isActive }) => (isActive ? 'active' : '')}
-              >
-                Contact
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/sign-up"
-                className={({ isActive }) => (isActive ? 'active' : '')}
-              >
-                SignUp
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/login"
-                className={({ isActive }) => (isActive ? 'active' : '')}
-              >
-                Login
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/mainpage"
-                className={({ isActive }) => (isActive ? 'active' : '')}
-              >
-                MainPage
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        {/* Redirect the root path to /login */}
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/mainpage" element={<MainPage />} />
