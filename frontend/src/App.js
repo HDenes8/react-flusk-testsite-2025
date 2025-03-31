4402aa85cf035cf893ca12e57bf2fbaeb4e82ff6
HDenes <denes.horvath.email@gmail.com>
2025-03-31 21:07:17 +0200

    just some little changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Layout from './components/Layout';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import ProjectsPage from './pages/ProjectsPage';
+import Projects from './pages/projects';
 import './App.css';
 
 function App() {
@@ -19,6 +20,11 @@ function App() {
         <Route path="/mainpage" element={
           <Layout>
             <ProjectsPage />
+          </Layout>
+        } />
+        <Route path="/projects" element={
+          <Layout>
+            <Projects />
           </Layout>
         } />
       </Routes>
